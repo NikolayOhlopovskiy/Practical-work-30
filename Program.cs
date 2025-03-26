7fd423c569d812f3731a662e91ec436fe90f55af
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2025-03-26 18:17:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add ( new Russia("Петя"));
+            people.Add(new Ucrine("Петро"));
+            people.Add(new English("Pete"));
+            foreach (Person person in people)
+                person.SayHello();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Russia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Russia : Person
+    {
+        public Russia(string name) { Name = name; }
+        public override void SayHello()
+        { Console.WriteLine($"{Name} говорит: Привет "); }
+    }
+}
